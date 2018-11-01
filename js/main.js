75c004d04bf223b8863afd96827e3fc276b6e106
Madenityan <marinakorsyn@gmail.com>
2018-11-02 00:04:44 +0200

    added and fix some methods

--- js/main.js
+++ js/main.js
@@ -7,83 +7,104 @@
 
 'use strict';
 
-class tamaguchi {
+class Tamagotchi {
 
     constructor(name) {
         this.name = name;
         this.weight = 50;
-        this.hours = 8;
     }
 
     weightLoss() {
         this.weight = this.weight - 2;
     };
 
-
-    running (){
-       let timerId = setInterval(() => {
-           this.weightLoss();
-           console.log(this.weight + ' kg is your weight');
-        }, 3000);
-
-
-       setTimeout(function() {
-            clearInterval(timerId);
-            console.log(doggy.name +',' + ' your weight was lowered!');
-
-        }, 5000);
-    };
-
-    eat() {
-        let timerId = setInterval(() => {
-            console.log('Oh my God ' + 54 + ' kg is your weight');
-        }, 6000);
-
-        setTimeout(function() {
-            clearInterval(timerId);
-            console.log(doggy.name +',' + ' Stop! Your weight is too fast');
-        }, 7000);
+    sayHello () {
+        let userName = prompt('What is your name?', 'Enter your name');
+        if (userName) {
+            alert ('Glad to see you, ' + userName + ' ;)');
+        }
     }
 
-// isSated = false;
-    // isHealthy = false;
-    // isHappiness = false;
-    // isDrunk = false;
-    // isFree = false;
-    // isOver = false;
-
-// greeting () {
-    //     let userName = prompt('What is your name?', 'Enter your name');
-    //     if (userName) {
-    //         alert ('Glad to see you, ' + userName + ' ;)');
-    //         document.getElementByld("dog").src = "image/happy.jpg";
-    //     }
-    // };
 
     sleep(){
-        let sleeping = setInterval(() => {
-            let sleepHours = prompt('How many hours did you sleep?');
-            if (sleepHours) {
-                alert ('Get up enough to sleep, ' + doggy.name );
-            }
-        }, 8000);
-
-        setTimeout(function() {
-            clearInterval(sleeping);
-            console.log(doggy.name +',' + ' It\'s time to drink');
-        }, 9000);
+        let sleepy = confirm('Do you want to sleep' + ',' + this.name + '?');
+        if (sleepy) {
+            alert ('sleep a little bit, ' + this.name );
+        } else {
+            alert(this.name +',' + ' it\'s time to eat');
+        }
     }
 
 
-    // drink(){}
-    // runAway(){}
-    // died(){}
+    eat() {
+        this.weight = this.weight + 5;
+        console.log('Oh my God ' + this.weight + ' kg is your weight');
+        console.log(this.name +',' + ' Stop eating! Your weight is too fast. it\'s time to go for a walk');
+    }
 
+
+    run (){
+       this.weightLoss();
+       console.log(this.weight + ' kg is your weight');
+       console.log(this.name +',' + ' your weight was lowered!');
+    };
+
+
+    drink(){
+        let drinking = confirm('Do you want to drink' + ',' + this.name + '?');
+        alert(drinking);
+
+        if(drinking){
+            alert('Drink.....')
+        }
+    }
+
+    sick () {
+        alert(this.name + ','  + 'catch');
+        alert('amend .....');
+    }
+
+    died (){
+        alert('drugs do not help....');
+        alert(this.name + ','  + 'died' + ''+ ':(');
+    }
 }
 
+let petName = prompt('How to call a pet??', 'Enter name');
+if (petName) {
+    alert ('Hi my friend ' + ' ;)');
+}
 
-let doggy = new tamaguchi('Bim');
-doggy.running();
-doggy.eat();
-doggy.sleep();
+let doggy = new Tamagotchi(petName);
+
+setTimeout(() => {
+    doggy.sayHello();
+}, 1000);
+
+setTimeout(() => {
+    doggy.sleep();
+},2000);
+
+setTimeout(() => {
+    doggy.eat();
+},3000);
+
+setTimeout(() => {
+    doggy.run();
+},4000);
+
+setTimeout(() => {
+    doggy.drink();
+},5000);
+
+setTimeout(() => {
+    doggy.sick();
+},6000);
+setTimeout(() => {
+    doggy.died();
+},7000);
+
+
+
+
 
